a166eccb6b0580338cea16b97ab8af4287e838ce
G H Mahimaanvita <mahimagangavarapu@gmail.com>
2025-04-30 16:06:28 +0530

    fix pylance warnings

--- 3_chains/1_chains_basics.py
+++ 3_chains/1_chains_basics.py
@@ -1,6 +1,6 @@
 from dotenv import load_dotenv
-from langchain.prompts import ChatPromptTemplate
-from langchain.schema.output_parser import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
 from langchain_openai import ChatOpenAI
 
 # Load environment variables from .env
